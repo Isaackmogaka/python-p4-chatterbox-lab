d1201bafc821b62ed55b3f32764f251071295138
isaacmogaka <isaacmogaka2016@gmail.com>
2023-09-21 15:44:17 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -14,13 +14,66 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    # Retrieve all messages ordered by created_at
+    messages = Message.query.order_by(Message.created_at).all()
+    return jsonify([message.serialize() for message in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+
+# @app.route('/messages')
+# def messages():
+#     return ''
+
+# @app.route('/messages/<int:id>')
+# def messages_by_id(id):
+#     return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.json
+    body = data.get('body')
+    username = data.get('username')
+    
+    if body and username:
+        message = Message(body=body, username=username)
+        db.session.add(message)
+        db.session.commit()
+        return jsonify(message.serialize()), 201
+    else:
+        return jsonify({'error': 'Invalid request data'}), 400
+    
+
+
+    
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+    
+    data = request.json
+    body = data.get('body')
+    
+    if body:
+        message.body = body
+        db.session.commit()
+        return jsonify(message.serialize())
+    else:
+        return jsonify({'error': 'Invalid request data'}), 400
+    
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+    
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({'message': 'Message deleted'})
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
+from sqlalchemy import DateTime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,3 +14,12 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(200), nullable=True)
+    username = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.String(20), default=str(datetime.utcnow()))
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __init__(self, body, username):
+        self.body = body
+        self.username = username
+
